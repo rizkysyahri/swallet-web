31d6202119b9913cb0501f9425937999e79e7a0c
Rizky Syahri Ramadhan <114801375+rizkysyahri@users.noreply.github.com>
2024-08-08 13:53:12 +0700

    fix: incoming code (#11)
    * http remove
    
    * feat:alert button
    
    * fix: resolve code
    
    * fix: incoming code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,8 @@ import "./globals.css";
 import Providers from "@/components/Providers";
 import Navbar from "@/components/header/Navbar";
 import { Toaster } from "@/components/ui/toaster";
-import { MessageSquareWarning } from "lucide-react";
 import AllertButton from "@/components/AllertButton";
-<<<<<<< HEAD
-=======
 import Footer from "@/components/Footer";
->>>>>>> c944565ddc80204c88c1863567158b07dfc07041
 
 const inter = Inter({ subsets: ["latin"] });
 
