6ecc97acb8970400231895cae20ebc562fffb0cb
Rizky Syahri Ramadhan <ramadhancai@gmail.com>
2024-08-08 13:20:12 +0700

    release

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://swallet-backend-production.up.railway.app",
-  baseURL: "http://localhost:4000"
+  baseURL: "https://swallet-backend-production.up.railway.app",
 });
 
 export default axiosInstance;
