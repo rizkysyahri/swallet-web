578a154764298f4d6470c580c638db95d125726e
Rizky Syahri Ramadhan <ramadhancai@gmail.com>
2024-08-08 15:13:24 +0700

    fix: debug api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://swallet-backend-production.up.railway.app",
+  // baseURL: "https://swallet-backend-production.up.railway.app",
+  baseURL: "http://localhost:4000",
 });
 
 export default axiosInstance;
